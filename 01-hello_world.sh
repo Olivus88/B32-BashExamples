33aee3fd8696a78e6c7cd8332ca10161f3640028
Oli <Oli@>
2017-04-24 17:33:49 -0400

    Done with Hello world fle

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+echo "salut"
+#exit
+echo "toi"
+
+# -n = Prevenir un retour de ligne automatique
+echo -n "Vive"
+echo " Linux"
+
+#-e = Accepter les meta caracteres (\n,\t,...)
+echo -e "1\n2\n3"
+
+# $=Lorsque l'on veut la valeur de la variable (lecture)
+# sinon, on affecte dans la variable (sans le $)
+read -p "Quel âge avez-vous" age
+echo $age "ans! Vous paraissez jeune!"
