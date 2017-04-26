485d2961c53f630104d40fffcbf2c832c5c79590
Oli <Oli@>
2017-04-26 14:55:59 -0400

    3 sortes de boucle yeah!

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -2,3 +2,19 @@
 
 for ((i=0;i<5;i++))
 do
+	echo $i
+done
+
+resultat=o
+while test $resultat = "o"
+do
+	read -p "Entrez une lettre : " resultat
+
+done
+
+maListe=`ls`
+
+for element in $maListe
+do
+	echo $element
+done
