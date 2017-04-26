14b0e10b509db283687c85f0f5de5b565c38e3d5
Oli <Oli@>
2017-04-26 16:43:30 -0400

    Exercice 3 fini

--- Exercice 3.sh
+++ Exercice 3.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+FaireEspace(){
+	nb=$1
+	i=0
+
+	while test $i -lt $nb
+	do
+		i=$(($i+1))
+		echo -n " "
+	done
+}
+
+
+for ((i=0;i<30;i++))
+do
+	clear
+	FaireEspace $i
+	echo "@"
+	sleep 0.1
+done
+
+clear
+FaireEspace 30
+echo "BOOOM"
+
+
