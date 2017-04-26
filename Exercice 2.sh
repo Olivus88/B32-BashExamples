e612d38739e8098aa83702bda38f45cab12ddd06
Oli <Oli@>
2017-04-26 16:00:49 -0400

    Exercice 2 afficher un menu

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+
+FctAfficher() {
+	clear
+	echo -e  "a Reaffiche le menu \nb Reaffiche le menu \nq Quitter\nVotre choix:"
+	read Choix
+}
+FctAfficher
+while test $Choix != "q"
+do
+	FctAfficher
+done 
+exit
